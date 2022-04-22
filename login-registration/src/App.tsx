5134b69c87187821787d0db770675bcb059226a2
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2022-04-22 15:50:11 +0300

    feat: add import RegistrationConfirmation

--- login-registration/src/App.tsx
+++ login-registration/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
+import RegistrationConfirmation from './pages/RegistrationConfirmation';
 
 function App() {
   return (
