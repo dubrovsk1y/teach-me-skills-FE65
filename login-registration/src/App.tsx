e333227c6ae1b6672e021625c2d6891f94e2e4d7
Yuriy Dubrovskiy <jr.dubrovskiy@gmail.com>
2022-04-24 01:21:20 +0300

    refactor: delete Login page, Registration page, Title component

--- login-registration/src/App.tsx
+++ login-registration/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import Login from './pages/Login';
-import Registration from './pages/Registration';
 import RegistrationConfirmation from './pages/RegistrationConfirmation';
 
 function App() {
--- login-registration/src/components/Title/Title.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Title.css'
-
-const Title = (props: any) => {
-    return <h3 className={props.className}>{props.text}</h3>
-}
-
-export default Title
